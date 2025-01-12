1e33a15f65580f13478417c19b1b073934d52548
Seeyong <seeyong987@gmail.com>
2025-01-12 16:53:57 +0900

    initial commit

---
+++ src/components/testResult/ResultButtonGroup.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import CopyToClipboard from "react-copy-to-clipboard";
+import { LinkOutlined, RedoOutlined, HomeOutlined } from "@ant-design/icons";
+import { base_url } from "../../App";
+import { useNavigate } from "react-router-dom";
+import styles from "./resultButtonGroup.module.css";
+
+const ResultButtonGroup = ({ testParam, resultParam }) => {
+  const navigate = useNavigate();
+  const onClickRedoButton = () => {
+    navigate(`/${testParam}`);
+  };
+
+  const onClickGoHomeButton = () => {
+    navigate("/");
+  };
+  return (
+    <div className={styles.mainDiv}>
+      <div className={styles.upperDiv}>
+        <CopyToClipboard
+          text={`${base_url}/${testParam}/result/${resultParam}`}
+        >
+          <button
+            className={styles.upperButton}
+            onClick={() => alert("URL이 복사되었습니다.")}
+          >
+            <LinkOutlined />
+            &nbsp; 링크 복사
+          </button>
+        </CopyToClipboard>
+        <button className={styles.upperButton} onClick={onClickRedoButton}>
+          <RedoOutlined />
+          &nbsp; 다시 하기
+        </button>
+      </div>
+      <div className={styles.bottomDiv}>
+        <button className={styles.bottomButton} onClick={onClickGoHomeButton}>
+          <HomeOutlined />
+          &nbsp; 다른 테스트 하러 가기
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default ResultButtonGroup;
---
+++ src/page/TestResult.jsx
@@ -0,0 +1,54 @@
+import { useNavigate, useParams } from "react-router-dom";
+import ResultButtonGroup from "../components/testResult/ResultButtonGroup";
+import ResultThumbnailList from "../components/testResult/ResultThumbnailList";
+import ShareButtonGroup from "../components/testResult/ShareButtonGroup";
+import TestResultRenderer from "../components/testResult/TestResultRenderer";
+import { useEffect, useState } from "react";
+import { TESTS } from "../data/TESTS";
+
+function TestResult() {
+  const navigate = useNavigate();
+  const { testParam, resultParam } = useParams();
+  const [renderResultInfo, setRenderResultInfo] = useState({});
+  const [renderTestInfo, setRenderTestInfo] = useState({});
+  useEffect(() => {
+    const testInfo = TESTS.find((test) => test.info.mainUrl === testParam);
+    if (!testInfo) {
+      alert("존재하지 않는 테스트입니다!");
+      navigate("/");
+    }
+    setRenderTestInfo(testInfo);
+
+    const resultInfo = testInfo?.results?.find(
+      (result) => result.query === resultParam
+    );
+    if (!resultInfo) {
+      alert("존재하지 않는 결과값입니다!");
+      navigate(`/${testInfo?.info?.mainUrl}`);
+    }
+    setRenderResultInfo(resultInfo);
+  }, [testParam, resultParam, navigate]);
+  return (
+    <div>
+      <TestResultRenderer renderResultInfo={renderResultInfo} />
+      <ShareButtonGroup
+        testParam={testParam}
+        resultParam={resultParam}
+        renderTestInfo={renderTestInfo}
+      />
+      <ResultButtonGroup
+        testParam={testParam}
+        resultParam={resultParam}
+        renderTestInfo={renderTestInfo}
+      />
+      <ResultThumbnailList testParam={testParam} />
+    </div>
+  );
+}
+
+// 1. Intro -> Quiz -> Loading -> Result
+// 2. Direct Access
+
+// /personalColor/result/ISFP
+
+export default TestResult;
