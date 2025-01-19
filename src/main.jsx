a7a876d7060b52ba381603ad923a1d6ed280d6db
cobaetoo <cobaetoo@gmail.com>
2025-01-19 20:39:19 +0900

    snap

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,36 @@ import { StrictMode } from "react";
 import { createRoot, hydrateRoot } from "react-dom/client";
 import App from "./App.jsx";
 import Layout from "./Layout.jsx";
+import { HelmetProvider } from "react-helmet-async";
 
 const rootElement = document.getElementById("root");
 if (rootElement.hasChildNodes()) {
   hydrateRoot(
     rootElement,
-    <StrictMode>
-      <Layout>
-        <App />
-      </Layout>
-    </StrictMode>
+    <HelmetProvider>
+      <StrictMode>
+        <Layout>
+          <App />
+        </Layout>
+      </StrictMode>
+    </HelmetProvider>
   );
 } else {
   createRoot(rootElement).render(
-    <StrictMode>
-      <Layout>
-        <App />
-      </Layout>
-    </StrictMode>
+    <HelmetProvider>
+      <StrictMode>
+        <Layout>
+          <App />
+        </Layout>
+      </StrictMode>
+    </HelmetProvider>
   );
 }
+
+// createRoot(document.getElementById("root")).render(
+//   <StrictMode>
+//     <Layout>
+//       <App />
+//     </Layout>
+//   </StrictMode>
+// );
