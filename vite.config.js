06bce39980a6810ef2bc44f2459de319e231e210
cobaetoo <cobaetoo@gmail.com>
2025-01-19 21:28:16 +0900

    pupee

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import prerender from "@prerenderer/rollup-plugin";
+import PuppeteerRenderer from "@prerenderer/renderer-puppeteer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    prerender({
+      routes: [
+        "/",
+        "/personalColor",
+        "/personalColorEng",
+        "/personalColorEng/result/ESTJ",
+      ],
+      renderer: PuppeteerRenderer,
+      server: {
+        port: 3000,
+        host: "localhost",
+      },
+      rendererOptions: {
+        maxConcurrentRoutes: 1,
+        renderAfterTime: 500,
+      },
+      postProcess(renderedRoute) {
+        renderedRoute.html = renderedRoute.html
+          .replace(/http:/i, "https:")
+          .replace(
+            /(https:\/\/)?(localhost|127\.0\.0\.1):\d*/i,
+            "https://mbti-platform.shop/"
+          );
+      },
+    }),
+  ],
 });
