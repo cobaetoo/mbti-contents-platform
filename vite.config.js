8893463220d7bcc41c0c46117bbf59da6e70e06c
cobaetoo <cobaetoo@gmail.com>
2025-01-22 23:43:35 +0900

    add Gitbub Actions yml for CI/CD

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
+// CI/CD
+// Continuous Integration / Continuous Deployment
+// Gitbub Actions
+//  -> Github에서 우리에게 원격 컴퓨터(배포용) -> 명령
+// .github/workflows/main.yml
+
+// 1. 코드 작성/수정
+// 2. Github Repo 코드 커밋/푸시
+// 3. 프로젝트 빌드 -> 빌드파일
+// 4. S3에 빌드파일 업로드
+// 5. CloudFront Invalidation
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import prerender from "@prerenderer/rollup-plugin";
