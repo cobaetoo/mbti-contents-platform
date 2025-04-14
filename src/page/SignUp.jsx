a281746041900c1ca591c2b4ad2506c89d152a59
Seeyong <seeyong987@gmail.com>
2025-04-14 13:56:52 +0900

    test log for mode

--- src/page/SignUp.jsx
+++ src/page/SignUp.jsx
@@ -20,7 +20,7 @@ const SignUp = () => {
   };
   return (
     <div>
-      {console.log(import.meta.env.MODE)}
+      {console.log("MODE", import.meta.env.MODE)}
       <h1>소셜 로그인</h1>
       <h3>간편하게 로그인해보세요</h3>
       <div>
